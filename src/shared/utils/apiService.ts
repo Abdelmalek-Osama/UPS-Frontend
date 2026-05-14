4701b06acb7471c5d6329208a91e195039646fda
LailaH.I. <lailahatem@gmail.com>
2026-05-14 15:46:46 +0300

    prod base url

--- src/shared/utils/apiService.ts
+++ src/shared/utils/apiService.ts
@@ -46,9 +46,9 @@ function cleanExpiredCache(): void {
 }
 
 //const API_BASE_URL = 'https://localhost:5001/api/';
-const API_BASE_URL = 'https://fw3.soft-trend.com:8883/api/';
-////const API_BASE_URL = "https://dairoot.duckdns.org:5050/api"
-//const API_BASE_URL = "https://tele-dairot.com:5050/api/";
+//const API_BASE_URL = 'https://fw3.soft-trend.com:8883/api/';
+//const API_BASE_URL = "https://dairoot.duckdns.org:5050/api"
+const API_BASE_URL = "https://tele-dairot.com:5050/api/";
 //const API_BASE_URL = "https://tele-dairot.com/swagger/index.html";
 //const API_BASE_URL = "https://tele-dairot.com:5050/api";
 
